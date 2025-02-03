65ad830a4b99cb64e71643bb95056306de4b8147
Lucas De La Fuente <lucasdlf17@gmail.com>
2025-02-03 11:52:38 -0300

    metacritic.js fetching

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import icon from "./assets/icon.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image
+        source={icon}
+        style={{ width: 100, height: 100, resizeMode: "center" }}
+      />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#09f',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#09f",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
